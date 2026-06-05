4d9ce833018331c141625008e6fa6a422028bbf7
bohdanty999 <bohdanty1@gmail.com>
2026-06-05 22:39:34 +0100

    hw finish

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,3 +1,17 @@
-export getImagesByQuery(query){
-    
-}
+import axios from 'axios';
+
+const API_KEY = '56165322-4eccb7c267e8fea85df72b379';
+
+export const getImagesByQuery = query => {
+  return axios
+    .get('https://pixabay.com/api/', {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data);
+};
